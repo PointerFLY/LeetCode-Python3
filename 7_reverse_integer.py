0c9bffff2da7029c2ed2af1d806033b40fc6eb0d
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-13 13:19:46 +0000

    Complete 7

---
+++ 7_reverse_integer.py
@@ -0,0 +1,33 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        max = 2**31
+        flag = 1
+        if x < 0:
+            x = -x
+            flag = -1
+
+        ret = 0
+        while x > 0:
+            ret *= 10
+            ret += x % 10;
+            x = int(x / 10)
+
+            if ret > max:
+                return 0
+
+        return flag * ret
+
+
+s = Solution()
+assert s.reverse(123) == 321
+assert s.reverse(-123) == -321
+assert s.reverse(120) == 21
+assert s.reverse(1534236469) == 0
+
+
+
+
