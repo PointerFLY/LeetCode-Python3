23c88583dab7d3fec44fd71fd7a85b3c1e0ec9a1
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-13 12:53:47 +0000

    Polish code for 1

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -8,9 +8,10 @@ class Solution:
         dic = dict()
         for idx, num in enumerate(nums, start=0):
             match = dic.get(target - num)
-            if match != None:
+            if match is not None:
                 return [match, idx]
             dic[num] = idx
+
 
 assert Solution().twoSum([2, 7, 11, 15], 9) == [0, 1]
 assert Solution().twoSum([3, 2, 4], 6) == [1, 2]
