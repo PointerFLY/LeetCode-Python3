4c8b23166aee43ef2c2cd71fff52a36373440482
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-15 23:07:08 +0000

    complete 14

---
+++ 14_longest_common_prefix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ''
+
+        prefix = strs.pop()
+        while strs:
+            str = strs.pop()
+            min_length = min(len(prefix), len(str))
+            for idx in range(0, min_length):
+                if prefix[idx] != str[idx]:
+                    prefix = prefix[0:idx]
+                    break
+            # When len(str) < len(prefix) && passed for-loop conditions
+            prefix = prefix[0:min(len(prefix), min_length)]
+
+        return prefix
+
+s = Solution()
+strs1 = ['aaa', 'aa', 'aaa']
+strs2 = ['', 'b']
+assert s.longestCommonPrefix(strs1) == 'aa'
+assert s.longestCommonPrefix(strs2) == ''
