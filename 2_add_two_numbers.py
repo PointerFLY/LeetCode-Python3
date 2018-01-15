491f81c218049a13c53db3a8b71a5427641e753f
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-15 23:08:52 +0000

    Complete 2

--- 2_add_two_numbers.py
+++ 2_add_two_numbers.py
@@ -4,6 +4,22 @@ class ListNode:
         self.val = x
         self.next = None
 
+    def __eq__(self, other):
+        self_next = self
+        other_next = other
+        while self_next is not None and other_next is not None:
+            if self_next.val != other_next.val:
+                return False
+            self_next = self_next.next
+            other_next = other_next.next
+
+        if (self_next is not None and other_next is None) \
+                or (self_next is None and other_next is not None):
+            return False
+
+        return True
+
+
 class Solution:
     def addTwoNumbers(self, l1, l2):
         """
@@ -11,30 +27,44 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        res = ListNode(7)
-        res.next = ListNode(0)
-        res.next.next = ListNode(8)
+        l1_node = l1
+        l2_node = l2
 
-        l_node = l1
-        r_node = l2
+        sum_node = None
+        head = None
+        carry = 0
 
-        sum = None
+        while l1_node is not None or l2_node is not None or carry != 0:
+            sum = carry
+            if l1_node is not None:
+                sum += l1_node.val
+                l1_node = l1_node.next
+            if l2_node is not None:
+                sum += l2_node.val
+                l2_node = l2_node.next
 
-        while l_node is not None or r_node is not None:
-            if l_node is not None:
-                pass
-            if r_node is not None:
-                pass
+            carry = int(sum / 10)
+            sum = sum % 10
+
+            if head is None:
+                sum_node = ListNode(sum)
+                head = sum_node
+            else:
+                sum_node.next = ListNode(sum)
+                sum_node = sum_node.next
+
+        return head
 
 
-s = Solution()
 l1 = ListNode(2)
 l1.next = ListNode(4)
 l1.next.next = ListNode(3)
 l2 = ListNode(5)
 l2.next = ListNode(6)
 l2.next.next = ListNode(4)
-res = ListNode(7)
-res.next = ListNode(0)
-res.next.next = ListNode(8)
-assert s.addTwoNumbers(l1, l2) == res
+expect = ListNode(7)
+expect.next = ListNode(0)
+expect.next.next = ListNode(8)
+s = Solution()
+answer = s.addTwoNumbers(l1, l2)
+assert answer == expect
