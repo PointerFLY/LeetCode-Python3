4556c9447c0ae2b805b25f04f67700dbd1efb528
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-16 14:45:53 +0000

    Variable name change 'pairs' to 'maps'

--- 13_roman_to_integer.py
+++ 13_roman_to_integer.py
@@ -4,7 +4,7 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        pairs = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10,
+        maps = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10,
                  'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
                  'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
 
@@ -13,8 +13,8 @@ class Solution:
         str_len = len(s)
 
         while idx < str_len:
-            v1 = pairs.get(s[idx])
-            v2 = pairs.get(s[idx:idx + 2]) if idx < str_len - 1 else None
+            v1 = maps.get(s[idx])
+            v2 = maps.get(s[idx:idx + 2]) if idx < str_len - 1 else None
 
             if v2 is not None:
                 sum_ += v2
