c0310206f48e00d842568f51e371ef5172999f8e
PointerFLY <linghao.michael.ma@gmail.com>
2018-01-20 10:39:51 +0000

    Undo 32

--- 32_longest_valid_parantheses.py
+++ 32_longest_valid_parantheses.py
@@ -1,35 +1,7 @@
 class Solution:
-    def letterCombinations(self, digits):
+    def longestValidParentheses(self, s):
         """
-        :type digits: str
-        :rtype: List[str]
+        :type s: str
+        :rtype: int
         """
-        maps = {
-            2: ['a', 'b', 'c'],
-            3: ['d', 'e', 'f'],
-            4: ['g', 'h', 'i'],
-            5: ['j', 'k', 'l'],
-            6: ['m', 'n', 'o'],
-            7: ['p', 'q', 'r', 's'],
-            8: ['t', 'u', 'v'],
-            9: ['w', 'x', 'y', 'z']
-        }
-
-        ret = list()
-        for char in digits:
-            letters = maps[int(char)]
-            if not ret:
-                ret = letters
-            else:
-                temp = ret
-                ret = list()
-                for idx in range(0, len(temp)):
-                    for letter in letters:
-                        ret.append(temp[idx] + letter)
-
-        return ret
-
-
-s = Solution()
-assert s.letterCombinations([]) == []
-assert s.letterCombinations('23') == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        
